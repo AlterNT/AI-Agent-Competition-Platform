050eaadb15a68156af81bfc15cf95f49cc4ab398
cookies-xor-cream <aryageza@gmail.com>
2022-08-08 10:26:52 +0800

    put python client into a class

--- example/Client/main.py
+++ example/Client/main.py
@@ -3,55 +3,69 @@ import subprocess
 import requests
 import json
 from subprocess import Popen, PIPE
+from pathlib import Path
 
-PATH = os.path.dirname(os.path.abspath(__file__))
+class Client:
+    PATH = (Path(os.path.abspath(__file__)) / '..').resolve()
+    ACCEPTED_FILETYPES = ['py', 'java']
 
-def load_agent():
-    # get list of all filenames in the Agents folder
-    agent_filenames = os.listdir(PATH + '/Agents')
-    
-    # print list of agent filenames to choose
-    print('AGENTS: ')
-    for i in range(len(agent_filenames)):
-        print(f'{i}. {agent_filenames[i]}')
-        
-    # requrest user to input the number of the agent atm
-    agent_index = int(input('\nCHOOSE AGENT: '))
-    agent_filename = agent_filenames[agent_index]
-    agent_filepath = PATH + '/Agents/' + agent_filename
-    
-    # if the agent is a java file will compile the file and communicate with it
-    if agent_filepath[-4:] == 'java':
-        subprocess.run(f'javac "{agent_filepath}"', shell=True)
-        agent_process = Popen(['java', agent_filepath], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    
-    # if the agent is a python file (none currently)
-    elif agent_filepath[-2:] == 'py':
-        agent_process = Popen(['python3', agent_filepath], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        
-    return agent_filename, agent_process
-        
-def load_game():
-    data = requests.get('http://localhost:8080/games')
-    games = json.loads(data.json())
-    
-    print('GAMES:')
-    for i in range(len(games)):
-        print(f'{i}. {games[i]["name"]}')
-    
-    game_index = int(input('\nCHOOSE GAME: '))
-    game = games[game_index]
+    def __init__(self, server_url='http://localhost:8080'):
+        self.game = None
+        self.agent_process = None
+        self.agent_filepath = None
+        self.server_url = server_url
 
-    return game
+    def execute_agent(self, agent_filepath):
+        # if the agent is a java file will compile the file and communicate with it
+        if agent_filepath.name.endswith('.java'):
+            subprocess.run(f'javac "{agent_filepath}"', shell=True)
+            self.agent_process = Popen(['java', agent_filepath], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        # if the agent is a python file (none currently)
+        elif  agent_filepath.name.endswith('.py'):
+            self.agent_process = Popen(['python3', agent_filepath], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        self.agent_filepath = agent_filepath
+
+    def load_agent(self):
+        # get list of all filenames in the Agents folder
+        # all_filenames = os.listdir(Client.PATH / 'Agents')
+        agents_dir = Client.PATH / 'Agents'
+
+        agent_filenames = []
+        for filetype in Client.ACCEPTED_FILETYPES:
+            files_of_type = list(agents_dir.glob(f'*.{filetype}'))
+            agent_filenames.extend(files_of_type)
+
+        # print list of agent filenames to choose
+        print('AGENTS: ')
+        for i, file in enumerate(agent_filenames):
+            print(f'{i}. {file.name}')
+
+        # requrest user to input the number of the agent atm
+        agent_index = int(input('\nCHOOSE AGENT: '))
+        agent_filename = agent_filenames[agent_index]
+        agent_filepath = Client.PATH / 'Agents' / agent_filename
+
+        self.execute_agent(agent_filepath)
+
+    def load_game(self):
+        data = requests.get(f'{self.server_url}/games')
+        games = json.loads(data.json())
+
+        print('GAMES:')
+        for i, game in enumerate(games):
+            print(f'{i}. {game["name"]}')
+
+        game_index = int(input('\nCHOOSE GAME: '))
+        self.game = games[game_index]
 
 def main():
-    game = load_game()
-    agent_filename, agent_process = load_agent()
-    
-    print(f'Playing {game["name"]} using agent {agent_filename}')
-    
-    
-    
+    game_client = Client()
+    game_client.load_game()
+    game_client.load_agent()
+
+    print(f'Playing {game_client.game["name"]} using agent {game_client.agent_filepath.name}')
 
 if __name__ == '__main__':
     main()
