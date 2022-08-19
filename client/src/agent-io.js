d617d5fda4d7dda6a92c7ea3625d4d3d65b61fef
BaxterCooper <baxtery.cooper@gmail.com>
2022-08-19 03:10:13 +0100

    Agent-io setup and love letter agent files created

---
+++ client/src/agent-io.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const prompt = require('prompt-sync')({ sigint: true })
+const fs = require('fs')
+const child_process = require('child_process')
+
+const ACCEPTED_FILETYPES = ['py', 'java']
+class AgentIO {
+    constructor() {
+
+    }
+
+    loadAgent() {
+        const agentsDIR = path.resolve(__dirname + '/agents')
+        const agentFilenames = []
+
+        for (const filename of fs.readdirSync(agentsDIR)) {
+            for (const filetype of ACCEPTED_FILETYPES) {
+                if (filename.endsWith(filetype)) {
+                    agentFilenames.push(filename)
+                }
+            }
+        }
+        
+        console.log('AGENTS: ')
+        for (const i in agentFilenames) {
+            console.log(`${i}. ${agentFilenames[i]}`)
+        }
+        
+        const agentIndex = parseInt(prompt('SELECT AGENT NUMBER: '))
+        const agentFilepath = path.resolve(agentsDIR + '/' + agentFilenames[agentIndex])
+
+        this.executeAgent(agentFilepath)
+    }
+
+    executeAgent(agentFilepath) {
+        let agent = null
+
+        if (agentFilepath.endsWith('py')) {
+            agent = child_process.spawn('python', [agentFilepath])
+
+        } else if (agentFilepath.endsWith('java')) {
+            child_process.exec('javac', [agentFilepath])
+            agent = child_process.spawn('java', [agentFilepath])
+        }
+
+        agent.stdout.on('data', (data) => {
+            console.log(`${data}`)
+        })
+
+        agent.stderr.on('data', (data) => {
+            console.log(`${data}`)
+        })
+    }
+
+    run() {
+        this.loadAgent()
+    }
+}
+
+const agentIO = new AgentIO()
+agentIO.run()
+
