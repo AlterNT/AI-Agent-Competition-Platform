2f839c52f83180a55c9770426d337f3df3a62ac1
BaxterCooper <baxtery.cooper@gmail.com>
2022-08-19 15:51:43 +0800

    express API created

--- server/api.js
+++ server/api.js
@@ -1,3 +1,26 @@
-class Api {
+import express from 'express'
+
+const app = express()
+app.listen(
+    8080,
+    () => { console.log('listening at http://localhost:8080') }
+)
+
+// return list of available games to be played
+app.get('/api/games', (req, res) => {
+    res.json()
+})
+
+// return gamestate view for agent
+app.get('api/game', (req, res) => {
+    const { agentToken } = req.params
     
-}
+    res.json()
+})
+
+// receive move played by an agent
+app.post('api/game', (req, res) => {
+    const { agentToken } = req.params
+})
+
+
