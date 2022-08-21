466a79252e00a9b5f7b9aed469e71d00279cc56a
BaxterCooper <baxtery.cooper@gmail.com>
2022-08-21 09:14:20 +0800

    added joinLobby and getGameState methods

--- client/src/server-handler.js
+++ client/src/server-handler.js
@@ -2,17 +2,38 @@ const fetch = require('node-fetch')
 
 const SERVER_API_ENDPOINT = 'http://localhost:8080/api'
 class ServerHandler {
-    constructor() {
+    constructor(agentToken, gameID) {
         this.serverAPI = SERVER_API_ENDPOINT
+        this.agentToken = agentToken
+        this.gameID = gameID
     }
 
-    startGame(agentToken, gameID) {
+    joinLobby() {
         const body = {
-            agentToken: agentToken,
-            gameID: gameID
+            agentToken: this.agentToken,
+            gameID: this.gameID
         }
 
-        const response = await fetch(this.serverAPI, { method: 'POST', body: body });
-        console.log(response)
+        const response = await fetch(this.serverAPI + '/join-lobby', { method: 'GET', body: body });
+
+        // handle error if could not join lobby
+    }
+
+    getGameState(agentToken) {
+        const body = {
+            agentToken: this.agentToken
+        }
+
+        const response = await fetch(this.serverAPI + '/game', { method: 'GET', body: body });
+        
+        // handle error if could not request game state
+    }
+
+    run() {
+        this.joinLobby()
+        
+        while (true) {
+            
+        }
     }
 }
