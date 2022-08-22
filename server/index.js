65d513a0a73c5517da51868e0054460a09c8d012
Nathan Phillis <nathanphillis1@gmail.com>
2022-08-22 13:54:49 +0800

    Basic game class structure.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 import Neode from 'neode';
 import Models from './models/index.js';
-import 'process';
+import LobbyManager from './lobby-manager.js';
+import Game from './game/game.js';
+import PaperScissorsRock from './game/psr.js';
 
-class Server {
-    constructor() {
-        this.db_instance = Neode.fromEnv().with(Models);
-    }
+export default class Server {
+
+    /** @type {Server} */
+    static instance;
+
+    /** @type {Neode} */
+    //db_instance = Neode.fromEnv().with(Models);
+    /** @type {LobbyManager} */
+    lobbyManager;
+
+    /** @type {[String]} */
+    games;
+
+    /** @type {typeof Game}} */
+    Game = PaperScissorsRock;
 
     async handleClose() {
     }
 
     async run() {
+        console.log(this.Game.maxPlayers);
     }
 }
 
