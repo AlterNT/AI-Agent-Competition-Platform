5238a4d541734bff86a13d96fe053f022d3ea8c7
cookies-xor-cream <aryageza@gmail.com>
2022-08-28 18:51:45 +0800

    Cleaning up GameManager code

--- server/game-manager.js
+++ server/game-manager.js
@@ -2,39 +2,31 @@ import GameInstance from './game-instance.js';
 
 export default class GameManager {
 
-    /** @type {Map<Number, GameInstance>} */
-    games = new Map();
-    /** @type {Map<String, Number>} */
-    playerMap = new Map();
+    /** @type {Map<String, GameInstance>} */
+    gameMap = new Map();
 
     async createGame(userTokens, numBots, debug) {
-        let gameId = 0;
-        // Find the first unused id.
-        while (this.lobbies.has(gameId)) gameId++;
-
-        for (let token of userTokens) {
-            this.playerMap.set(token, gameId);
-        }
-
         const game = new GameInstance(userTokens, numBots, debug);
         await game.start();
-        this.games.set(gameId, game);
+        userTokens.forEach((token) => this.games.set(token, game));
     }
 
+    /**
+     * @param {String} token
+     * @returns {GameInstance}
+     */
     async getGameInstance(token) {
-        if (!this.playerMap.has(token)) {
+        if (!this.gameMap.has(token)) {
             console.error(`User ${token} requested for a game, but is not in one!`);
             return null;
         }
-        return this.games.get(this.playerMap.get(token));
+        return this.games.get(this.gameMap.get(token));
     }
 
+    /**
+     * @param {GameInstance} game
+     */
     async removeGame(game) {
-        // Cheese way of getting the id of the game.
-        const id = this.playerMap.get(game.userTokens[0]);
-        for (const token of game.userTokens) {
-            this.playerMap.delete(token);
-        }
-        this.games.delete(id);
+        game.userTokens.forEach((token) => this.gameMap.delete(token));
     }
 }
