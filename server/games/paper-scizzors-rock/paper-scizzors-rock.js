1a65dd8b1c62ed993098b675f061d2182eb177d4
Baxter Cooper <baxtery.cooper@gmail.com>
2022-09-05 15:49:53 +0800

    paper scizzors rock game completed

--- server/games/paper-scizzors-rock/paper-scizzors-rock.js
+++ server/games/paper-scizzors-rock/paper-scizzors-rock.js
@@ -5,6 +5,12 @@ class PaperScizzorsRock {
     static SCIZZORS = 'SCIZZORS'
     static ROCK = 'ROCK'
 
+    ACTIONS = {
+        'PAPER': 1,
+        'SCIZZORS': 2,
+        'ROCK': 3,
+    }
+
     constructor(agentTokens) {
         this.agents = []
         for (const agentToken of agentTokens) {
@@ -13,6 +19,7 @@ class PaperScizzorsRock {
         this.pending = null
         this.resolve = null
         this.moves = []
+        this.result = null
     }
 
     async main() {
@@ -24,7 +31,18 @@ class PaperScizzorsRock {
             this.moves.push(move)
         }
 
-        console.log(this.moves)
+        const move0 = ACTIONS[this.moves[0]]
+        const move1 = ACTIONS[this.moves[1]]
+
+        if (move0 == move1) {
+            this.result = null
+
+        } else if (move0 + 1 % 3 == move1 % 3) {
+            this.result = this.agents[0]
+
+        } else {
+            this.result = this.agents[1]
+        } 
     }
 }
 
