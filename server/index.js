082d4bd630946e76796b7d3b71ae2befd311eb95
Baxter Cooper <baxtery.cooper@gmail.com>
2022-09-05 21:08:33 +0800

    removed unused files and code

--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
 import API from './api.js'
-import LobbyManager from './lobby-manager.js'
 
 async function main() {
-    // initialise Lobby Manager
-    const lobbyManager = new LobbyManager()
-
     // intialise API
     const api = new API()
     api.run()
-
-
-    
 }
 
 main()
