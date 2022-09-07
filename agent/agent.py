08f2b43c1a6cdfcca9896ba84a57a9b2bd7c754e
cookies-xor-cream <aryageza@gmail.com>
2022-09-07 11:39:26 +0800

    python agent is now robust to server crashing or network disconnecting

--- agent/agent.py
+++ agent/agent.py
@@ -1,5 +1,6 @@
 import random
 import time
+import requests
 
 from agentIO import AgentIO
 
@@ -11,25 +12,34 @@ MOVES = {
 
 def main():
     agentIO = AgentIO(f'token{random.randrange(1000)}')
-    
+
     while True:
-        print('joining lobby')
-        agentIO.join_lobby()
-        print('joined lobby')
-        
-        print('waiting for turn...')
-        while not agentIO.turn():
+        print('attempting to join lobby')
+        while not agentIO.join_lobby():
+            print('server unreachable, reattempting to join lobby:')
+            print('\tcheck that the game server is running and you have a working connection')
             time.sleep(1)
-            print('waiting for turn...')
-            
-        print('making move')
-        random_move_int = random.randint(1, 3)
-        move = MOVES[random_move_int]
-        print(f'move = {move}')
-        
-        print('playing move')
-        agentIO.send_action('MOVE', move)
-        print('played move')
+
+        print('joined lobby')
+
+        try:
+            print('will now await turn')
+            while not agentIO.turn():
+                print('waiting for turn...')
+                time.sleep(1)
+
+            print('making move')
+            random_move_int = random.randint(1, 3)
+            move = MOVES[random_move_int]
+            print(f'move = {move}')
+
+            print('playing move')
+            agentIO.send_action('MOVE', move)
+            print('played move')
+
+            print('finished game\n')
+        except requests.exceptions.ConnectionError:
+            print('disconected from server... aborting and starting a new game\n')
 
 if __name__ == '__main__':
     main()
--- agent/agentIO.py
+++ agent/agentIO.py
@@ -10,27 +10,36 @@ class AgentIO:
         return f'{self.server_url}/{path}'
 
     def join_lobby(self):
-        response = requests.post(self.server_path('api/join'), json={
-            'agentToken': self.agentToken,
-            'gameID': 'paper-scissors-rock'
-        })
-        response_json = response.json()
-        return response_json['success']
+        try:
+            response = requests.post(self.server_path('api/join'), json={
+                'agentToken': self.agentToken,
+                'gameID': 'paper-scissors-rock'
+            })
+            response_json = response.json()
+            return response_json['success']
+        except requests.exceptions.ConnectionError:
+            return False
 
     def send_action(self, type, value):
-        response = requests.post(self.server_path('api/action'), json={
-            'agentToken': self.agentToken,
-            'action': {
-                'type': type,
-                'value': value
-            }
-        })
-        return response
+        try:
+            response = requests.post(self.server_path('api/action'), json={
+                'agentToken': self.agentToken,
+                'action': {
+                    'type': type,
+                    'value': value
+                }
+            })
+            return response
+        except requests.exceptions.ConnectionError:
+            raise requests.exceptions.ConnectionError
 
     def see(self):
         return self.send_action('METHOD', 'see') # ['gamestate']
 
     def turn(self):
-        response = requests.get(self.server_path('api/turn'), { 'agentToken': self.agentToken })
-        response_json = response.json()
-        return response_json['turn']
+        try:
+            response = requests.get(self.server_path('api/turn'), { 'agentToken': self.agentToken })
+            response_json = response.json()
+            return response_json['turn']
+        except requests.exceptions.ConnectionError:
+            raise requests.exceptions.ConnectionError
