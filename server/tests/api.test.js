4811b513e5c6135753bb2ae1ee040404eb8ccbee
cookies-xor-cream <aryageza@gmail.com>
2022-10-02 17:44:49 +0800

    API tests: available games + database disabled errors

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -3,13 +3,58 @@ import API from "../api.js";
 import Database from "../database.js"
 
 beforeAll(async () => {
-    await Database.init()
-    await API.init()
+    process.env.DATABASE_ENABLED = true;
+    await API.init();
 });
 
-describe('Testing API (api.js):', () => {
-    test('Demo API Test', async () => {
-        const response = await request(API.app).get('/api/available-games')
-        expect(JSON.parse(response.text)).toBe(true);
+beforeEach(async () => {
+    await Database.init();
+})
+
+describe('API', () => {
+    it('Persistent Endpoints Disabled', async () => {
+        const persistentEndpoints = [
+            '/api/agents',
+            '/api/bots',
+            '/api/count-game-pages',
+            '/api/agent-games',
+            '/api/top-winrate',
+            '/api/most-improved',
+            '/api/most-improving',
+        ];
+
+        for (const endpoint of persistentEndpoints) {
+            const response = await request(API.app).get(endpoint);
+            const responseBody = JSON.parse(response.text);
+            const returnedKeys = Object.keys(responseBody);
+            expect(returnedKeys.length).toBe(1);
+
+            const returnedObject = responseBody[returnedKeys];
+            expect(returnedObject).toEqual({ error: 'Database not implemented' });
+        }
+    });
+
+    // @TODO: fill out these tests
+    xdescribe('Endpoints With Arguments', () => {
+        it('/api/games', async () => {
+        });
+        it('/api/game', async () => {
+        });
+        it('/api/winrate', async () => {
+        });
+        it('/api/improvement', async () => {
+        });
+        it('/api/improvement-rate', async () => {
+        });
+    });
+
+    describe('Static Returns', () => {
+        it('Available Games', async () => {
+            const response = await request(API.app).get('/api/available-games');
+            const responseBody = JSON.parse(response.text);
+
+            const gameIDs = ['love-letter', 'paper-scissors-rock'];
+            expect(responseBody.gameIDs).toEqual(gameIDs);
+        });
     });
 });
