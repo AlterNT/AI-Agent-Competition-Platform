51cce7a3378bff5603b33d99239d5e727cbb6a8e
cookies-xor-cream <aryageza@gmail.com>
2022-10-04 15:57:56 +0800

    made config file fallback to config.json5.example (idiot proofing)

--- server/config.js
+++ server/config.js
@@ -1,12 +1,25 @@
 import { cosmiconfigSync } from 'cosmiconfig';
 import JSON5 from 'json5';
 
+const readConfig = (searchPath) => {
+    return cosmiconfigSync('config', {
+        searchPlaces: [
+            searchPath
+        ],
+        loaders: {
+            '.json5': (_, data) => JSON5.parse(data),
+            '.example': (_, data) => JSON5.parse(data),
+        }
+    }).search()?.config;
+}
+
 /** @type {{database: {enabled: Boolean, protocol: String, host: String, port: Number, username: String, password},games: Object.<String, {path: String, settings: {maxPlayers: Number, minPlayers: Number, timeout: Number, failClause: String, bot: String}, tournament: {}}>}} */
-export default cosmiconfigSync('config', {
-    searchPlaces: [
-        'config.json5'
-    ],
-    loaders: {
-        '.json5': (_, data) => JSON5.parse(data),
-    }
-}).search().config;
+let config = readConfig('config.json5')
+
+if (!config) {
+    console.error('`config.json5` not found, defaulting to `config.json5.example`')
+    config = readConfig('config.json5.example')
+    console.log(config)
+}
+
+export default config;
