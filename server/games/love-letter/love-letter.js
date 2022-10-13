3f93121ee77b2e4641448200d222c53b3cf76766
Nathan Phillis <nathanphillis1@gmail.com>
2022-10-13 16:10:36 +0800

    Attempt #3

--- server/games/love-letter/love-letter.js
+++ server/games/love-letter/love-letter.js
@@ -85,7 +85,6 @@ class LoveLetter extends IGame {
 
                     let act = await agent.playCard(topCard)
                     try {
-                        
                         this.stream.write(gameState.update(act, topCard) + '\n');
                     } catch {
                         this.stream.write(
